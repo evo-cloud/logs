36fa274be070d971a177e49ca4e555339279af12
EaseWay Hu <easeway@gmail.com>
2021-04-13 22:49:40 -0700

    http instrumentation

---
+++ pkg/http/client.go
@@ -0,0 +1,25 @@
+package http
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/evo-cloud/logs"
+)
+
+// UpdateRequest injects span information into request header.
+func UpdateRequest(r *http.Request) {
+	UpdateHeader(r.Context(), r.Header)
+}
+
+// UpdateHeader updates HTTP header.
+func UpdateHeader(ctx context.Context, header http.Header) {
+	logger := logs.Use(ctx)
+	spanInfo := logger.SpanInfo()
+	if traceID := spanInfo.TraceID(); traceID != "" {
+		header.Add(B3TraceIDHeader, traceID)
+	}
+	if spanID := spanInfo.SpanID(); spanID != "" {
+		header.Add(B3SpanIDHeader, spanID)
+	}
+}
